069c5c2674246467d5aba4703bf1056a87677d6a
DELL <sangyeop0529@gmail.com>
2025-08-04 15:20:09 +0900

    filter

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ export default class App extends Component {
   constructor() {
     super({
       state: {
-        inputText: "",
+        fruits: [
+          { name: "Apple", price: 1000 },
+          { name: "Banana", price: 2000 },
+          { name: "Cherry", price: 3000 },
+        ],
       },
     });
   }
   render() {
-    this.el.classList.add("search");
+    console.log(this.state.fruits);
     this.el.innerHTML = /* html */ `
-      <input/>
-      <button>Click!</button>
+    <h1>Fruits</h1>
+    <ul>
+      ${this.state.fruits
+        .filter((fruit) => fruit.price < 3000)
+        .map((fruit) => `<li>${fruit.name}</li>`)
+        .join("")}
+    </ul>
     `;
-
-    const inputEl = this.el.querySelector("input");
-    inputEl.addEventListener("input", () => {
-      this.state.inputText = inputEl.value;
-    });
-
-    const buttonEl = this.el.querySelector("button");
-    buttonEl.addEventListener("click", () => {
-      console.log(this.state.inputText);
-    });
   }
 }
