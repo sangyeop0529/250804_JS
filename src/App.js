9904dfc16f90acf5151d84f41ff12ba3c4dc9bd0
DELL <sangyeop0529@gmail.com>
2025-08-05 08:44:22 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import { Component } from "./core/core";
+import TheHeader from "./components/TheHeader";
 
 export default class App extends Component {
-  constructor() {
-    super({
-      state: {
-        fruits: [
-          { name: "Apple", price: 1000 },
-          { name: "Banana", price: 2000 },
-          { name: "Cherry", price: 3000 },
-        ],
-      },
-    });
-  }
   render() {
-    console.log(this.state.fruits);
-    this.el.innerHTML = /* html */ `
-    <h1>Fruits</h1>
-    <ul>
-      ${this.state.fruits
-        .filter((fruit) => fruit.price < 3000)
-        .map((fruit) => `<li>${fruit.name}</li>`)
-        .join("")}
-    </ul>
-    `;
+    const routerView = document.createElement("router-view");
+    this.el.append(new TheHeader().el, routerView);
   }
 }
--- src/core/core.js
+++ src/core/core.js
@@ -1,12 +1,23 @@
 ///// Component /////
 export class Component {
   constructor(payload = {}) {
-    const { tagName = "div", state = {} } = payload;
+    const { tagName = "div", state = {}, props = {} } = payload;
     this.el = document.createElement(tagName);
     this.state = state;
+    this.props = props;
     this.render();
   }
   render() {
     // ...
   }
+}
+
+///// Router /////
+export function createRouter(router) {
+  return function () {
+    window.addEventListener("popstate", () => {
+      routeRender(router);
+    });
+    routeRender();
+  };
 }
